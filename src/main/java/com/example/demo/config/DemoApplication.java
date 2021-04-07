8da8f5b98804a109b4a38233f4efe2e73820a347
SorryImSoBad <52279190+SorryImSoBad@users.noreply.github.com>
2021-04-07 14:50:07 +0800

    2-6 Hello Word接口

--- src/main/java/com/example/demo/config/DemoApplication.java
+++ src/main/java/com/example/demo/config/DemoApplication.java
@@ -1,8 +1,10 @@
-package com.example.demo;
+package com.example.demo.config;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
+@ComponentScan("com.example")
 @SpringBootApplication
 public class DemoApplication {
 
