4e11674cbe7852cd52b19a6f2d647b89fdbf4634
SorryImSoBad <52279190+SorryImSoBad@users.noreply.github.com>
2021-04-09 17:02:27 +0800

    3-4 mybatis使用实例

--- doc/all.sql
+++ doc/all.sql
@@ -1,11 +1,10 @@
-drop table if exists `db_goods`;
-create table `db_goods` (
+drop table if exists `test`;
+create table `test` (
   `id` bigint unsigned not null AUTO_INCREMENT comment '主键',
-  `goods_id` int(11) unsigned not null comment '商品id',
-  `goods_title` varchar(225) not null DEFAULT '' comment '商品名称',
-  `goods_price` decimal(10,2) not null comment '商品价格',
-  `create_time` timestamp not null DEFAULT CURRENT_TIMESTAMP comment '创建时间',
-  `update_time` timestamp not null DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP  comment '更新时间',
+  `name` varchar(225) not null DEFAULT '' comment '名称',
+  `password` varchar(225) not null comment '密码',
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci COMMENT='商品表';
-select * from db_goods;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci COMMENT='测试';
+select * from `test`;
+
+insert into `test` (id, name, password) values (1, '测试', 'password');
--- src/main/java/com/example/demo/controller/TestController.java
+++ src/main/java/com/example/demo/controller/TestController.java
@@ -1,13 +1,27 @@
 package com.example.demo.controller;
 
+import com.example.demo.domain.Test;
+import com.example.demo.service.TestService;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
+import java.util.List;
 
 @RestController
 public class TestController {
 
+    @Resource
+    private TestService testService;
+
     @RequestMapping("/hello")
     public String hello(){
         return "Hello Word1";
+    }
+
+    @GetMapping("/test/list")
+    public List<Test> list() {
+        return testService.list();
     }
 }
