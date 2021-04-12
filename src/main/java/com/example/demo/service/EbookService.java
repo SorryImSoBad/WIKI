3379700f3918d0024bb9a1c3c95f45ec9f77f150
SorryImSoBad <52279190+SorryImSoBad@users.noreply.github.com>
2021-04-12 14:36:10 +0800

    3-6 ebook查询接口

---
+++ src/main/java/com/example/demo/service/EbookService.java
@@ -0,0 +1,19 @@
+package com.example.demo.service;
+
+import com.example.demo.domain.Ebook;
+import com.example.demo.mapper.EbookMapper;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
+import java.util.List;
+
+@Service
+public class EbookService {
+
+    @Resource
+    private EbookMapper ebookMapper;
+
+    public List<Ebook> list(){
+        return ebookMapper.selectByExample(null);
+    }
+}
