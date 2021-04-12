10b8ab34e5d1b93a89113a1273ab19bf67f469c1
SorryImSoBad <52279190+SorryImSoBad@users.noreply.github.com>
2021-04-12 14:46:52 +0800

    3-6 增加通用返回类CommonResp

--- src/main/java/com/example/demo/controller/EbookController.java
+++ src/main/java/com/example/demo/controller/EbookController.java
@@ -1,6 +1,7 @@
 package com.example.demo.controller;
 
 import com.example.demo.domain.Ebook;
+import com.example.demo.resp.CommonResp;
 import com.example.demo.service.EbookService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,7 +18,10 @@ public class EbookController {
     private EbookService ebookService;
 
     @GetMapping("/list")
-    public List<Ebook> list() {
-        return ebookService.list();
+    public CommonResp list() {
+        CommonResp<List<Ebook>> resp = new CommonResp<>();
+        List<Ebook> list= ebookService.list();
+        resp.setContent(list);
+        return resp;
     }
 }
