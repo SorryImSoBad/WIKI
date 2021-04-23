418e90fd81a38f72d1d22508d36bed10970e4fea
SorryImSoBad <52279190+SorryImSoBad@users.noreply.github.com>
2021-04-23 16:08:16 +0800

    6-4 使用pagehelper实现分页

--- src/main/java/com/example/demo/service/EbookService.java
+++ src/main/java/com/example/demo/service/EbookService.java
@@ -6,6 +6,10 @@ import com.example.demo.mapper.EbookMapper;
 import com.example.demo.req.EbookReq;
 import com.example.demo.resp.EbookResp;
 import com.example.demo.util.CopyUtil;
+import com.github.pagehelper.PageHelper;
+import com.github.pagehelper.PageInfo;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 import org.springframework.util.ObjectUtils;
 
@@ -15,6 +19,8 @@ import java.util.List;
 @Service
 public class EbookService {
 
+    private static final Logger LOG = LoggerFactory.getLogger(EbookService.class);
+
     @Resource
     private EbookMapper ebookMapper;
 
@@ -23,7 +29,13 @@ public class EbookService {
         EbookExample.Criteria criteria = ebookExample.createCriteria();
         if (!ObjectUtils.isEmpty(req.getName()))
             criteria.andNameLike('%'+req.getName()+'%');
+        PageHelper.startPage(1,3);
         List<Ebook> ebookList = ebookMapper.selectByExample(ebookExample);
+
+        PageInfo<Ebook> pageInfo = new PageInfo<>(ebookList);
+        LOG.info("总行数：{}", pageInfo.getTotal());
+        LOG.info("总页数：{}", pageInfo.getPages());
+
 
 //        List<EbookResp> respList = new ArrayList<>();
 //        for(Ebook ebook: ebookList){
