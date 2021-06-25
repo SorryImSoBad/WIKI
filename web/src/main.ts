72856caccfb9309e8293f708361a6199ba349e5e
SorryImSoBad <52279190+SorryImSoBad@users.noreply.github.com>
2021-06-25 17:24:15 +0800

    9.11 前后端增加登录拦截 修复BUG 前后端存储token统一用string

--- web/src/main.ts
+++ web/src/main.ts
@@ -6,6 +6,7 @@ import Antd from 'ant-design-vue'
 import 'ant-design-vue/dist/antd.css'
 import axios from "axios";
 import * as Icons from '@ant-design/icons-vue';
+import {Tool} from "@/util/tool";
 
 const app = createApp(App);
 app.use(store).use(router).use(Antd).mount('#app');
@@ -20,6 +21,11 @@ for (const i in icons) {
  */
 axios.interceptors.request.use(function (config) {
     console.log('请求参数：', config);
+    const token = store.state.user.token;
+    if (Tool.isNotEmpty(token)) {
+        config.headers.token = token
+        console.log("请求headers增加token:", token);
+    }
     return config;
 }, error => {
     return Promise.reject(error);
