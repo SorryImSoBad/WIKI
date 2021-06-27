1e296da4d138e3fcf591b8eb686f2e290453c0a1
SorryImSoBad <52279190+SorryImSoBad@users.noreply.github.com>
2021-06-27 19:01:56 +0800

    10-5 定时任务实例

--- src/main/java/com/example/demo/config/DemoApplication.java
+++ src/main/java/com/example/demo/config/DemoApplication.java
@@ -1,17 +1,26 @@
 package com.example.demo.config;
 
 import org.mybatis.spring.annotation.MapperScan;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.core.env.Environment;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @ComponentScan("com.example")
 @SpringBootApplication
 @MapperScan("com.example.demo.mapper")
+@EnableScheduling
 public class DemoApplication {
 
+    private static final Logger LOG = LoggerFactory.getLogger(DemoApplication.class);
+
     public static void main(String[] args) {
-        SpringApplication.run(DemoApplication.class, args);
+        Environment env = SpringApplication.run(DemoApplication.class, args).getEnvironment();
+        LOG.info("启动成功！");
+        LOG.info("地址：\thttp://127.0.0.1:{}", env.getProperty("server.port"));
     }
 
 }
