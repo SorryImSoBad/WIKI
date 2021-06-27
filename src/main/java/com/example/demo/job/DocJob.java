208d319ecdb6bdd9e1523738add995340fa37cb7
SorryImSoBad <52279190+SorryImSoBad@users.noreply.github.com>
2021-06-27 19:54:46 +0800

    10-7 增加日志流水号，方便生产运维

--- src/main/java/com/example/demo/job/DocJob.java
+++ src/main/java/com/example/demo/job/DocJob.java
@@ -1,8 +1,10 @@
 package com.example.demo.job;
 
 import com.example.demo.service.DocService;
+import com.example.demo.util.SnowFlake;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.slf4j.MDC;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
@@ -16,6 +18,9 @@ public class DocJob {
     @Resource
     private DocService docService;
 
+    @Resource
+    private SnowFlake snowFlake;
+
     /**
      * 自定义cron表达式跑批
      * 只有等上一次执行完成，下一次才会在下一个时间点执行，错过就错过
@@ -23,6 +28,8 @@ public class DocJob {
      */
     @Scheduled(cron = "0/30 * * * * ?")
     public void cron() {
+        //增加日志流水号
+        MDC.put("LOG_ID", String.valueOf(snowFlake.nextId()));
         LOG.info("更新电子书下的文档开始");
         long start = System.currentTimeMillis();
         docService.updateEbookInfo();
